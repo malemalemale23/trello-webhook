a1bacd877697273e9c10c784db6f7eb02880ce3b
malemalemale23 <malemale2323@hotmail.com>
2026-03-23 15:39:18 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ import axios from "axios";
 const app = express();
 app.use(express.json());
 
-const key = "YOUR_KEY";
-const token = "YOUR_TOKEN";
+const key = "129f7d85d240ac6b419fb20531bc3e08";
+const token = "ATTA4bd8efd28a0174d4c6497d80739e9ef98dbc1063acd5dacf4dd0e90e9099852a70610460";
 
 // ===== cache =====
 let cachedLists = null;
